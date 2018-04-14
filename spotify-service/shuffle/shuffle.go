b46844ad0bb74e9c8a94184e33b0c23bc68f34b2
Christian Johansen <christian@cjohansen.no>
2018-04-15 00:27:44 +0200

    Working-ish distributed shuffle

--- spotify-service/shuffle/shuffle.go
+++ spotify-service/shuffle/shuffle.go
@@ -1,40 +1,101 @@
 package shuffle
 
-func IndexOf(coll []string, item string, start int) int {
-	for i := start; i < len(coll); i++ {
-		if coll[i] == item {
-			return i
-		}
+import (
+	"bytes"
+	"math/rand"
+	"sort"
+)
+
+type Track struct {
+	artist string
+	album  string
+	track  string
+	genre  string
+}
+
+type Collection []Track
+
+type GroupedCollection map[string]Collection
+
+type Bucket struct {
+	label       string
+	occurrences int
+}
+
+func (coll Collection) String() string {
+	var buffer bytes.Buffer
+
+	for _, track := range coll {
+		buffer.WriteString("\n")
+		buffer.WriteString(track.String())
 	}
 
-	for i := 0; i < start; i++ {
-		if coll[i] == item {
-			return i
+	return buffer.String()
+}
+
+func (track Track) String() string {
+	var buffer bytes.Buffer
+	buffer.WriteString("{")
+	buffer.WriteString("Artist: ")
+	buffer.WriteString(track.artist)
+	buffer.WriteString(", Album: ")
+	buffer.WriteString(track.album)
+	buffer.WriteString(", Track: ")
+	buffer.WriteString(track.track)
+	buffer.WriteString(", Genre: ")
+	buffer.WriteString(track.genre)
+	buffer.WriteString("}")
+	return buffer.String()
+}
+
+type byOccurrences []Bucket
+
+func (s byOccurrences) Len() int {
+	return len(s)
+}
+
+func (s byOccurrences) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s byOccurrences) Less(i, j int) bool {
+	return s[i].occurrences > s[j].occurrences
+}
+
+func IndexOf(haystack []string, needle string, start int) int {
+	l := len(haystack)
+
+	for i := start; i < start+l; i++ {
+		index := i % l
+		if haystack[index] == needle {
+			return index
 		}
 	}
 
 	return -1
 }
 
-func GroupBy(coll []map[string]string, key string) map[string][]map[string]string {
-	res := make(map[string][]map[string]string)
+func GroupBy(coll Collection, fn func(Track) string) GroupedCollection {
+	res := make(GroupedCollection)
 
-	for _, item := range coll {
-		if res[item[key]] == nil {
-			res[item[key]] = make([]map[string]string, 1)
-			res[item[key]][0] = item
+	for _, track := range coll {
+		key := fn(track)
+
+		if res[key] == nil {
+			res[key] = make(Collection, 1)
+			res[key][0] = track
 		} else {
-			res[item[key]] = append(res[item[key]], item)
+			res[key] = append(res[key], track)
 		}
 	}
 
 	return res
 }
 
-func Distribute(distribution []string, bucket string, items int) []string {
-	var stepSize float64 = float64(len(distribution)) / float64(items)
+func Distribute(distribution []string, bucket Bucket) []string {
+	var stepSize float64 = float64(len(distribution)) / float64(bucket.occurrences)
 	index := 0
-	n := items
+	n := bucket.occurrences
 	remainder := stepSize
 
 	for {
@@ -46,10 +107,73 @@ func Distribute(distribution []string, bucket string, items int) []string {
 			index = IndexOf(distribution, "", index)
 			n--
 			remainder -= stepSize
-			distribution[index] = bucket
+			distribution[index] = bucket.label
 		}
 
 		index++
 		remainder++
 	}
+}
+
+func shuffle(src Collection) Collection {
+	dest := make(Collection, len(src))
+	perm := rand.Perm(len(src))
+
+	for i, v := range perm {
+		dest[v] = src[i]
+	}
+
+	return dest
+}
+
+func bucketsByOccurrences(grouped GroupedCollection) []Bucket {
+	res := make([]Bucket, len(grouped))
+	i := 0
+
+	for bucket, bucketItems := range grouped {
+		res[i] = Bucket{bucket, len(bucketItems)}
+		i++
+	}
+
+	sort.Sort(byOccurrences(res))
+	return res
+}
+
+func FlattenDistribution(distribution []string, groups map[string]Collection) Collection {
+	coll := make(Collection, len(distribution))
+
+	for i, key := range distribution {
+		coll[i], groups[key] = groups[key][0], groups[key][1:]
+	}
+
+	return coll
+}
+
+func ShuffleBy(coll Collection, fns []func(Track) string) Collection {
+	res := make([]string, len(coll))
+
+	if len(fns) == 0 {
+		return shuffle(coll)
+	}
+
+	distributedGrouped := make(map[string]Collection)
+	fn := fns[0]
+	grouped := GroupBy(coll, fn)
+	buckets := bucketsByOccurrences(grouped)
+
+	for _, bucket := range buckets {
+		distributedGrouped[bucket.label] = ShuffleBy(grouped[bucket.label], fns[1:])
+		Distribute(res, bucket)
+	}
+
+	return FlattenDistribution(res, distributedGrouped)
+}
+
+func StartRandomly(coll Collection) Collection {
+	index := rand.Intn(len(coll))
+	return append(coll[index:], coll[:index]...)
+}
+
+func DistributedShuffle(coll Collection, fns []func(Track) string) Collection {
+	return StartRandomly(ShuffleBy(coll, fns))
 }
--- spotify-service/shuffle/shuffle_test.go
+++ spotify-service/shuffle/shuffle_test.go
@@ -2,8 +2,9 @@ package shuffle
 
 import (
 	"github.com/stretchr/testify/assert"
+	"math/rand"
 	"testing"
-	"testing/quick"
+	"time"
 )
 
 func TestIndexOf(t *testing.T) {
@@ -20,7 +21,7 @@ func TestIndexOf(t *testing.T) {
 	assert.Equal(t, IndexOf(coll, "Times", 6), 5)
 }
 
-func groupedItemCount(grouped map[string][]map[string]string) int {
+func (grouped GroupedCollection) len() int {
 	sum := 0
 
 	for _, value := range grouped {
@@ -31,19 +32,21 @@ func groupedItemCount(grouped map[string][]map[string]string) int {
 }
 
 func TestGroupBy(t *testing.T) {
-	coll := []map[string]string{
-		map[string]string{"id": "1", "fruit": "Banana"},
-		map[string]string{"id": "2", "fruit": "Apple"},
-		map[string]string{"id": "3", "fruit": "Banana"},
-		map[string]string{"id": "4", "fruit": "Orange"},
-		map[string]string{"id": "5", "fruit": "Grapes"},
-		map[string]string{"id": "6", "fruit": "Apple"},
-		map[string]string{"id": "7", "fruit": "Apple"},
-		map[string]string{"id": "8", "fruit": "Grapes"},
-		map[string]string{"id": "9", "fruit": "Orange"},
+	coll := Collection{
+		Track{artist: "Banana"},
+		Track{artist: "Apple"},
+		Track{artist: "Banana"},
+		Track{artist: "Orange"},
+		Track{artist: "Grapes"},
+		Track{artist: "Apple"},
+		Track{artist: "Apple"},
+		Track{artist: "Grapes"},
+		Track{artist: "Orange"},
 	}
 
-	grouped := GroupBy(coll, "fruit")
+	grouped := GroupBy(coll, func(track Track) string {
+		return track.artist
+	})
 
 	assert.Equal(t, 2, len(grouped["Banana"]))
 	assert.Equal(t, 3, len(grouped["Apple"]))
@@ -51,99 +54,106 @@ func TestGroupBy(t *testing.T) {
 	assert.Equal(t, 2, len(grouped["Grapes"]))
 }
 
-func TestGroupByProperties(t *testing.T) {
-	f := func(coll []map[string]string, key string) bool {
-		grouped := GroupBy(coll, key)
-		return len(coll) == groupedItemCount(grouped)
-	}
-
-	if err := quick.Check(f, nil); err != nil {
-		t.Error(err)
-	}
-}
-
 func TestDistribute(t *testing.T) {
 	assert.Equal(
 		t,
 		[]string{"A", "", "", "", "", "", "", "", "", ""},
-		Distribute(make([]string, 10), "A", 1),
+		Distribute(make([]string, 10), Bucket{"A", 1}),
 	)
 
 	assert.Equal(
 		t,
 		[]string{"A", "", "", "", "", "A", "", "", "", ""},
-		Distribute(make([]string, 10), "A", 2),
+		Distribute(make([]string, 10), Bucket{"A", 2}),
 	)
 
 	assert.Equal(
 		t,
 		[]string{"A", "", "", "", "A", "", "", "A", "", ""},
-		Distribute(make([]string, 10), "A", 3),
+		Distribute(make([]string, 10), Bucket{"A", 3}),
 	)
 
 	assert.Equal(
 		t,
 		[]string{"A", "", "", "A", "", "A", "", "", "A", ""},
-		Distribute(make([]string, 10), "A", 4),
+		Distribute(make([]string, 10), Bucket{"A", 4}),
 	)
 
 	assert.Equal(
 		t,
 		[]string{"A", "", "A", "", "A", "", "A", "", "A", ""},
-		Distribute(make([]string, 10), "A", 5),
+		Distribute(make([]string, 10), Bucket{"A", 5}),
 	)
 
 	assert.Equal(
 		t,
 		[]string{"A", "", "A", "", "A", "", "A", "A", "", "A"},
-		Distribute(make([]string, 10), "A", 6),
+		Distribute(make([]string, 10), Bucket{"A", 6}),
 	)
 
 	assert.Equal(
 		t,
 		[]string{"A", "", "A", "A", "", "A", "A", "", "A", "A"},
-		Distribute(make([]string, 10), "A", 7),
+		Distribute(make([]string, 10), Bucket{"A", 7}),
 	)
 
 	assert.Equal(
 		t,
 		[]string{"A", "", "A", "A", "A", "A", "", "A", "A", "A"},
-		Distribute(make([]string, 10), "A", 8),
+		Distribute(make([]string, 10), Bucket{"A", 8}),
 	)
 
 	assert.Equal(
 		t,
 		[]string{"A", "", "A", "A", "A", "A", "A", "A", "A", "A"},
-		Distribute(make([]string, 10), "A", 9),
+		Distribute(make([]string, 10), Bucket{"A", 9}),
 	)
 
 	assert.Equal(
 		t,
 		[]string{"A", "B", "A", "C", "A", "", "A", "B", "A", "C"},
-		Distribute([]string{"A", "B", "A", "", "A", "", "A", "B", "A", ""}, "C", 2),
+		Distribute([]string{"A", "B", "A", "", "A", "", "A", "B", "A", ""}, Bucket{"C", 2}),
 	)
 
 	assert.Equal(
 		t,
 		[]string{"A", "B", "A", "D", "A", "C", "A", "B", "A", "C"},
-		Distribute([]string{"A", "B", "A", "D", "A", "", "A", "B", "A", ""}, "C", 2),
+		Distribute([]string{"A", "B", "A", "D", "A", "", "A", "B", "A", ""}, Bucket{"C", 2}),
 	)
 
 	assert.Equal(
 		t,
 		[]string{"A", "B", "A", "C", "A", "C", "A", "B", "A", "D"},
-		Distribute([]string{"A", "B", "A", "", "A", "", "A", "B", "A", "D"}, "C", 2),
+		Distribute([]string{"A", "B", "A", "", "A", "", "A", "B", "A", "D"}, Bucket{"C", 2}),
 	)
+}
+
+func TestDistributedShuffle(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+
+	t.Log(DistributedShuffle(Collection{
+		Track{artist: "Nirvana", genre: "Grunge", track: "Smells Like Teen Spirit", album: "Nevermind"},
+		Track{artist: "Nirvana", genre: "Grunge", track: "Come as You Are", album: "Nevermind"},
+		Track{artist: "Nirvana", genre: "Grunge", track: "Heart-Shaped Box", album: "In Utero"},
+		Track{artist: "Nirvana", genre: "Grunge", track: "Dive", album: "Incesticide"},
+		Track{artist: "Pearl Jam", genre: "Grunge", track: "Once", album: "Ten"},
+		Track{artist: "Pearl Jam", genre: "Grunge", track: "Daughter", album: "VS"},
+		Track{artist: "Leviathan", genre: "Black_Metal", track: "Her Circle is the Noose", album: "True Traitor, True Whore"},
+		Track{artist: "Leviathan", genre: "Black_Metal", track: "The Smoke of Their Torment", album: "Scar Sighted"},
+		Track{artist: "Nile", genre: "Death_Metal", track: "Black Seeds of Vengeance", album: "Black Seeds of Vengeance"},
+		Track{artist: "Deathspell Omega", genre: "Black_Metal", track: "Hetomaisa", album: "Si Circumspice"},
+		Track{artist: "Execration", genre: "Death_Metal", track: "Eternal Recurrence", album: "Return to the Void"},
+	}, []func(Track) string{
+		func(c Track) string {
+			return c.artist
+		},
+	}))
+
+	t.Error("Oops")
 }
 
 func BenchmarkDistribute(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Distribute(make([]string, 10), "A", 5)
-	}
-}
-
-func BenchmarkDistributeRecursive(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		DistributeRecursive(make([]string, 10), "A", 5)
+		Distribute(make([]string, 10), Bucket{"A", 5})
 	}
 }
